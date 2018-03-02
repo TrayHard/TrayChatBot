e71c2f641ce4763792cdb939d33b4613761c9864
Tray <wwwtrayfrost@gmail.com>
2018-03-02 18:19:30 +0300

    Chat works.

--- TrayChatBot/MainForm.cs
+++ TrayChatBot/MainForm.cs
@@ -31,7 +31,13 @@ namespace TrayChatBot
         {
             InitializeComponent();
         }
-
+        // КНОПКИ
+        ////////////
+        /// <summary>
+        /// [кнопка] Connect
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void mf_btnConnect_Click(object sender, EventArgs e)
         {
             // Проверка, подключен ли пользователь уже
@@ -50,41 +56,82 @@ namespace TrayChatBot
             client.Connect();
             print("Connecting...");
         }
-
+        /// <summary>
+        /// [кнопка] Chat
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void mf_btnChat_Click(object sender, EventArgs e)
         {
+            // Проверка если подключен то пишет в чат
             if (client.IsConnected)
             {
-                print(mf_txtChatLineBox.Text);
+                print(client.TwitchUsername + ": " + mf_txtChatLineBox.Text);
                 client.SendMessage(mf_txtChatLineBox.Text);
             }
+            // Если нет, то выдает ошибку
             else
             {
-                print("Error! You are not connected!");
+                print("ERROR: You are not connected!");
             }
         }
-
+        /// <summary>
+        /// [кнопка] Disconnect
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void mf_btnDisconnect_Click(object sender, EventArgs e)
         {
             client.Disconnect();
         }
-
+        // ФУНКЦИИ
+        /////////////
+        /// <summary>
+        /// [handler] ПОДКЛЮЧЕНИЕ к серверу
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void onConnected(object sender, OnConnectedArgs e)
         {
+            CheckForIllegalCrossThreadCalls = false;
+
             client.OnDisconnected += new EventHandler<OnDisconnectedArgs>(onDisconnected);
             client.OnConnected -= onConnected;
-            CheckForIllegalCrossThreadCalls = false;
-            print("Connection established!\n");
+            
+            // Подключаемся к чату канала
             client.JoinChannel(mf_txtChannelName.Text);
-        }
+            print("Connection established!\n");
 
+            client.OnMessageReceived += new EventHandler<OnMessageReceivedArgs>(globalMessageRecieved);
+        }
+        /// <summary>
+        /// [handler] ОТКЛЮЧЕНИЕ от сервера
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         public void onDisconnected(object sender, OnDisconnectedArgs e)
         {
             CheckForIllegalCrossThreadCalls = false;
-            print("Disconnected!");
+
             client.OnDisconnected -= onDisconnected;
+            print("Disconnected!");
         }
-              
+        /// <summary>
+        /// [handler] Получение нового сообщения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void globalMessageRecieved(object sender, OnMessageReceivedArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+
+            print(e.ChatMessage.DisplayName + ": " + e.ChatMessage.Message);
+        }
+
+        /// <summary>
+        /// [функция] Написать в чатбокс
+        /// </summary>
+        /// <param name="text"></param>
         public void print(string text)
         {
             mf_ChatBox.AppendText(text + "\n");
